9c45a92c1600023849ca59a341fbd991e03856db
Matheus <matheusbrito1999@live.com>
2023-04-16 11:40:42 -0300

    cadastrar address junto do user

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,14 +3,16 @@ import { AppError } from "../handlers/errors.handler";
 import userRepository from "../repositories/user.repository";
 import userCategoriesRepository from "../repositories/user_categories.repository";
 import userSocialRepository from "../repositories/user_social.repository";
-import {generateToken} from '../utils/jsonwebtoken.util'
+import addressRepository from "../repositories/address.repository";
+import { generateToken } from '../utils/jsonwebtoken.util'
+
 
 class UserController {
   async create(request: Request, response: Response) {
-    const { name, username, password, user_type, document, email, profile_image, cel_phone, status, addressId, categories, social } = request.body;
+    const { name, username, password, user_type, document, email, profile_image, cel_phone, status, addressId, address, categories, social } = request.body;
 
-                  
-    if (!name || !username || !password || !user_type ) {
+
+    if (!name || !username || !password || !user_type) {
       const missingFields = [];
 
       if (!name) missingFields.push("name");
@@ -23,16 +25,51 @@ class UserController {
 
     const user = await userRepository.create({ name, username, password, user_type, document, email, profile_image, cel_phone, status, addressId });
 
-    if (categories && Array.isArray(categories)){
-        categories.forEach((el) => {
-          userCategoriesRepository.create({
-            category: el,
-            user: user.id
-          })
+    if (typeof address === 'object' && address !== null) {
+
+      const addr: AddressProps = address;
+
+      if (!addr.street || !addr.neighborhood || !addr.number || !addr.city || !addr.contry || !addr.zip_code) {
+        const missingFields = [];
+
+        if (!addr.street) missingFields.push("street");
+        if (!addr.neighborhood) missingFields.push("neighborhood");
+        if (!addr.number) missingFields.push("number");
+        if (!addr.contry) missingFields.push("contry");
+        if (!addr.city) missingFields.push("city");
+        if (!addr.zip_code) missingFields.push("zip_code");
+
+        throw new AppError(`Um ou mais campos do endereço não foram não enviados: ${missingFields.join(", ")}`);
+      }
+
+      const addressUser =
+        await addressRepository.create({
+          city: addr.city,
+          contry: addr.contry,
+          neighborhood: addr.neighborhood,
+          number: addr.number,
+          street: addr.street,
+          zip_code: addr.zip_code,
+          lat: addr.lat,
+          long: addr.long,
+          user: user.id
         });
+
+        if(addressUser){
+          user.addressId = addressUser.id;
+        }
     }
 
-    if (social && Array.isArray(social)){
+    if (categories && Array.isArray(categories)) {
+      categories.forEach((el) => {
+        userCategoriesRepository.create({
+          category: el,
+          user: user.id
+        })
+      });
+    }
+
+    if (social && Array.isArray(social)) {
       social.forEach((el) => {
         userSocialRepository.create({
           social: el.id,
@@ -40,16 +77,16 @@ class UserController {
           user: user.id
         })
       });
-  }
+    }
 
     return response.status(201).json(user);
   }
 
   async login(request: Request, response: Response) {
-    const {username, password} = request.body;
+    const { username, password } = request.body;
 
-                  
-    if ( !username || !password  ) {
+
+    if (!username || !password) {
       const missingFields = [];
 
       if (!username) missingFields.push("username");
@@ -57,22 +94,22 @@ class UserController {
 
       throw new AppError(`Um ou mais campos não enviados: ${missingFields.join(", ")}`);
     }
-    
+
     const user = await userRepository.findByUsername(username);
 
     if (!user) {
       throw new AppError("Usuario não encontrado", 401);
     }
 
-    if (user.password != password ) {
+    if (user.password != password) {
       throw new AppError("Usuario ou senha invalidos!", 401);
     }
 
     const token = await generateToken(user);
 
     var retorno = new Object({
-      "name":user.name,
-      "user_type":user.user_type,
+      "name": user.name,
+      "user_type": user.user_type,
       "token": token
     });
 
---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,11 @@
+interface AddressProps {
+    street: string;
+    neighborhood: string;
+    number: string;
+    contry: string;
+    city: string;
+    lat: string;
+    long: string;
+    zip_code: string;
+    user?: string;
+  }
