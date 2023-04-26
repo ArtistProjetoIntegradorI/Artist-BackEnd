5f47d92130ba39189b22da96c78ca53bf69e6c3c
Matheus Brito <matheus.brito@conecteme.com.vc>
2023-04-26 20:06:05 -0300

    disponibilizar pasta de midias

--- src/app.ts
+++ src/app.ts
@@ -8,12 +8,16 @@ import { router } from "./routes";
 import { handleAsyncErrors } from "./middlewares/errors.middleware";
 
 const app = express();
+const images = process.env.IMAGES_PATH || "";
+
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
 app.use("/api/v1", router);
+
+app.use("/api/v1/images", express.static(images));
 
 app.use(handleAsyncErrors);
 
